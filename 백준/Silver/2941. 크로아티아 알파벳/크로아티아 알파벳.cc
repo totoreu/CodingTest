4a878daf3516ec8cb36d82ae8963e325bbbc5d2b
God_win <96413770+totoreu@users.noreply.github.com>
2023-11-06 04:33:39 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+const std::vector<std::string> Croatia{"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+const bool Checker(std::string& Str)
+{
+    for (const std::string Alphabet : Croatia)
+    {
+        int FindIt = Str.find(Alphabet);
+        if (FindIt != std::string::npos)
+        {
+            Str.replace(Str.find(Alphabet), Alphabet.length(), Alphabet.length(),'.');
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    std::string Str;
+    std::cin >> Str;
+
+    int Count = 0;
+    while (Checker(Str)) 
+    {
+        Count++;
+    }
+
+    int Size = std::count_if(Str.begin(), Str.end(), 
+        [](const int x) 
+        {
+            return x != '.'; 
+        }
+    );
+
+    std::cout << Count + Size;
+
+    return 0;
+}
