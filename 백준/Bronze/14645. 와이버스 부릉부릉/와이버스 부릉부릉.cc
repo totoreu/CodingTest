7ec2695ae621b123f497328c1ee3a88a3535d0f6
God_win <96413770+totoreu@users.noreply.github.com>
2023-11-07 01:53:20 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() 
+{
+    int N = 0, K = 0;
+    std::cin >> N >> K;
+
+    int A = 0, B = 0;
+    for (int i = 0; i < N; ++i)
+    {
+        std::cin >> A >> B;
+    }
+    std::cout << "비와이";
+    return 0;
+}
