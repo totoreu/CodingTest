c3290ef58db6de5c91c964c6dd6d2faeaa35e4d9
God_win <96413770+totoreu@users.noreply.github.com>
2023-11-07 01:44:21 +0900

    [Bronze V] Title: Hook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() 
+{
+    printf("#  # #### #### #  #\n");
+    printf("#### #  # #  # # # \n");
+    printf("#### #  # #  # # # \n");
+    printf("#  # #### #### #  #\n");
+    return 0;
+}
