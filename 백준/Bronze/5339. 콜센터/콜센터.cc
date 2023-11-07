9561d36ffa262e7202f2880c2571c495b7d68d6e
God_win <96413770+totoreu@users.noreply.github.com>
2023-11-07 14:45:10 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "     /~\\" << std::endl;
+    std::cout << "    ( oo|" << std::endl;
+    std::cout << "    _\\=/_" << std::endl;
+    std::cout << "   /  _  \\" << std::endl;
+    std::cout << "  //|/.\\|\\\\" << std::endl;
+    std::cout << " ||  \\ /  ||" << std::endl;
+    std::cout << "============" << std::endl;
+    std::cout << "|          |" << std::endl;
+    std::cout << "|          |" << std::endl;
+    std::cout << "|          |" << std::endl;
+
+    return 0;
+}
