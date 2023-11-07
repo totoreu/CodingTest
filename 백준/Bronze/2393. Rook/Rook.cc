08cd5f2501b00a9e4a25d512410012014bcdc18c
God_win <96413770+totoreu@users.noreply.github.com>
2023-11-08 00:53:07 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    printf("  ___  ___  ___\n");
+    printf("  | |__| |__| |\n");
+    printf("  |           |\n");
+    printf("   \\_________/\n");
+    printf("    \\_______/\n");
+    printf("     |     |\n");
+    printf("     |     |\n");
+    printf("     |     |\n");
+    printf("     |     |\n");
+    printf("     |_____|\n");
+    printf("  __/       \\__\n");
+    printf(" /             \\\n");
+    printf("/_______________\\\n");
+
+    return 0;
+}
