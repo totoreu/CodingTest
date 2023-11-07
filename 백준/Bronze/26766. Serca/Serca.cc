5d92a642c4e6d0b1ef907a71cf1872aa7f27e73b
God_win <96413770+totoreu@users.noreply.github.com>
2023-11-08 00:43:57 +0900

    [Bronze V] Title: Serca, Time: 128 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+    size_t N = 0;
+    std::cin >> N;
+
+    for (size_t i = 0; i < N; ++i)
+    {
+        printf(" @@@   @@@ \n");
+        printf("@   @ @   @\n");
+        printf("@    @    @\n");
+        printf("@         @\n");
+        printf(" @       @ \n");
+        printf("  @     @  \n");
+        printf("   @   @   \n");
+        printf("    @ @    \n");
+        printf("     @     \n");
+    }
+
+    return 0;
+}
