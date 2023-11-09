944f61dc110873fb40fa8eac3096c8090a057c66
God_win <96413770+totoreu@users.noreply.github.com>
2023-11-09 19:11:48 +0900

    [Bronze I] Title: 캠핑, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/4796. 캠핑/캠핑.cc
+++ 백준/Bronze/4796. 캠핑/캠핑.cc
@@ -2,24 +2,18 @@
 
 int main()
 {
+    unsigned int L = 0, P = 0, V = 0;
     size_t Index = 1;
     while (true)
     {
-        unsigned int L = 0, P = 0, V = 0;
         std::cin >> L >> P >> V;
 
-        if (V == 0)
+        if (!V) 
             break;
 
         unsigned int Sum = (V / P) * L;
-
-        unsigned int Mod = 0;
-        Mod = V % P;
-
-        if (Mod <= L)
-            Sum += Mod;
-        else
-            Sum += L;
+        unsigned int Mod = V % P;
+        Sum += std::min(Mod, L);
         
         printf("Case %zu: %u\n", Index++, Sum);
     }
