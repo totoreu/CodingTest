c7acff51ea36f7a96f1aa9de82b88b20e078d2ca
God_win <96413770+totoreu@users.noreply.github.com>
2023-11-10 09:07:13 +0900

    [Bronze II] Title: 욱제는 효도쟁이야!!, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/14487. 욱제는 효도쟁이야！！/욱제는 효도쟁이야！！.cc
+++ 백준/Bronze/14487. 욱제는 효도쟁이야！！/욱제는 효도쟁이야！！.cc
@@ -1,26 +1,21 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
-#include <numeric>
 
 int main()
 {
     unsigned int N = 0;
     std::cin >> N;
 
-    std::vector<unsigned int> Distance;
+    unsigned int Sum = 0, MaxNumber = 0;
     for (size_t i = 0; i < N; ++i)
     {
         unsigned int Num = 0;
         std::cin >> Num;
-        Distance.emplace_back(Num);
+        Sum += Num;
+        if (MaxNumber < Num)
+            MaxNumber = Num;
     }
-    std::sort(Distance.begin(), Distance.end());
 
-    unsigned int Sum = 0;
-    Sum = std::accumulate(Distance.begin(), Distance.end() - 1, 0);
-
-    std::cout << Sum;
+    std::cout << Sum - MaxNumber;
 
     return 0;
 }
